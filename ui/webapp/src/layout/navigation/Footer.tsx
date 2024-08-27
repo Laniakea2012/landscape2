c921389e9c5b7b04afccfd10fb393ab8e59633d0
laixingyou <laixy7@gmail.com>
2024-08-27 17:27:54 +0800

    Feat: Add compass logo
    Signed-off-by: laixingyou <laixy7@gmail.com>

--- ui/webapp/src/layout/navigation/Footer.tsx
+++ ui/webapp/src/layout/navigation/Footer.tsx
@@ -92,7 +92,7 @@ const Footer = () => {
                     (!isUndefined(window.baseDS.footer!.links) && !isEmpty(window.baseDS.footer!.links))
                   }
                 >
-                  <div class="d-flex flex-column flex-lg-row justify-content-start align-items-lg-center w-100">
+                  <div class="d-flex gap-2 flex-column flex-lg-row justify-content-start align-items-lg-center w-100">
                     <Show when={!isUndefined(window.baseDS.footer!.logo)}>
                       <Show
                         when={!isUndefined(foundationLink())}
@@ -110,6 +110,11 @@ const Footer = () => {
                               logo={window.baseDS.footer!.logo!}
                               height={34}
                             />
+                          </div>
+                        </ExternalLink>
+                        <ExternalLink href={'https://oss-compass.org'}>
+                          <div class={styles.logoWrapper}>
+                            <SVGIcon kind={SVGIconKind.OssCompass} class={`me-1 ${styles.logo}`} />
                           </div>
                         </ExternalLink>
                       </Show>
@@ -131,16 +136,13 @@ const Footer = () => {
                 OpenAtom OpenHarmony（简称“OpenHarmony”）是由开放原子开源基金会（OpenAtom
                 Foundation）孵化及运营的开源项目
               </div>
-              {/* <div class={`pt-2 ${styles.legend}`}>
+              <div class={`pt-2 ${styles.legend}`}>
                 Powered by{' '}
-                <ExternalLink
-                  class="p-0 fw-semibold text-white text-underline"
-                  href="https://github.com/cncf/landscape2"
-                >
-                  CNCF interactive landscapes generator
+                <ExternalLink class="p-0 fw-semibold text-white text-underline" href="https://oss-compass.org">
+                  Oss-Compass
                 </ExternalLink>
                 .
-              </div> */}
+              </div>
             </div>
           </div>
         </div>
